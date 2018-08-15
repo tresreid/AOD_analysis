e511133469a40b8a9e85141072501d6538b6a85b
michael reid <mreid@cmslpc42.fnal.gov>
2018-08-15 14:42:25 -0500

    incutseff now with maluable histograms plus some extra things

--- analysis/move.py
+++ analysis/move.py
@@ -1,8 +1,14 @@
 import os, sys
-rootprefix="root://cmseos.fnal.gov//store/user/mreid/standaloneComp/"
-with open('filelist/M_60_dM_10_mZD_150.txt') as f:
+#rootprefix="root://cmseos.fnal.gov//store/user/mreid/standaloneComp/test"
+with open('filelist/move.txt') as f:
         rootfiles1 = f.readlines()
-commands = ["xrdcp root://cmseos.fnal.gov//store/user/mreid/standaloneComp/%s root://cmseos.fnal.gov//store/user/mreid/standaloneComp/iDM/M_60_dM_10_mZD_150/%s"%(x.strip(),x.strip()) for x in rootfiles1]
+
+commands10000 = ["xrdcp root://cmseos.fnal.gov//store/user/mreid/standaloneComp/iDM/redone/%s root://cmseos.fnal.gov//store/user/mreid/iDM/lifetime_10000mm/%s"%(x.strip(),x.strip()) for x in rootfiles1 if ("ctau-10000_" in x and "_AOD" in x)]
+commands1000 = ["xrdcp root://cmseos.fnal.gov//store/user/mreid/standaloneComp/iDM/redone/%s root://cmseos.fnal.gov//store/user/mreid/iDM/lifetime_1000mm/%s"%(x.strip(),x.strip()) for x in rootfiles1 if ("ctau-1000_" in x and "_AOD" in x)]
+commands100 = ["xrdcp root://cmseos.fnal.gov//store/user/mreid/standaloneComp/iDM/redone/%s root://cmseos.fnal.gov//store/user/mreid/iDM/lifetime_100mm/%s"%(x.strip(),x.strip()) for x in rootfiles1 if ("ctau-100_" in x and "_AOD" in x)]
+commands10 = ["xrdcp root://cmseos.fnal.gov//store/user/mreid/standaloneComp/iDM/redone/%s root://cmseos.fnal.gov//store/user/mreid/iDM/lifetime_10mm/%s"%(x.strip(),x.strip()) for x in rootfiles1 if ("ctau-10_" in x and "_AOD" in x)]
+commands1 = ["xrdcp root://cmseos.fnal.gov//store/user/mreid/standaloneComp/iDM/redone/%s root://cmseos.fnal.gov//store/user/mreid/iDM/lifetime_1mm/%s"%(x.strip(),x.strip()) for x in rootfiles1 if ("ctau-1_" in x and "_AOD" in x)]
+commands = commands1+commands10+commands100+commands1000+commands10000
 print commands
 for cmd in commands:
 	os.system(cmd)
